dcccb486f56e429630357989fea5fb5fce725b1c
Brett Mulligan <brett.mulligan@gmail.com>
2016-10-15 17:36:58 -0700

    Startup text and port argument

--- ss.cpp
+++ ss.cpp
@@ -1,9 +1,55 @@
 // ss.cpp - stepping stone program listens for requests and
 //          transfers file via addresses in chainfile
+// Author: K. Brett Mulligan
+// Date: Sep 2016
+// CSU - Comp Sci
+// CS457 - Networks
+// Dr. Indrajit Ray
+// License: MIT
+// References : Beej's Guide to Socket Programming
+//              https://beej.us/guide/bgnet/
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+using namespace std;
 
 
 int main (int argc, char* argv[]) {
 
+    cout << "Starting stepping stone..." << endl;
+
+    int opt = 0;
+    string portval;
+
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+            case 'p':
+                portval = optarg;
+                break;
+            case '?':
+                if (optopt == 'p') {
+                    cerr << "Option -" << optopt << " requires an argument." << endl;
+                } else {
+                    cerr << "Unknown option " << optopt << endl;
+                }
+
+            default:
+                cerr << "getopt error: default ... aborting!" << endl;
+                abort();
+        }
+
+    }
 
 
     return 0;
