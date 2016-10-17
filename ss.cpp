b2edd0e2a8adda74fd7bf8c941f5e6978cf03a1c
Brett Mulligan <brett.mulligan@gmail.com>
2016-10-16 18:47:51 -0700

    Added listening on port

--- ss.cpp
+++ ss.cpp
@@ -24,6 +24,15 @@
 
 using namespace std;
 
+const int MAX_CHARS = 255;
+const int BACKLOG = 1;
+
+int start_listening(int portreq);
+string get_ip();
+
+
+
+
 
 int main (int argc, char* argv[]) {
 
@@ -51,6 +60,138 @@ int main (int argc, char* argv[]) {
 
     }
 
+    char hostname[MAX_CHARS];
 
+    gethostname(hostname, MAX_CHARS);
+    cout << hostname << " " << portval <<  endl;
+
+    start_listening(atoi(portval.c_str()));
+    
     return 0;
+}
+
+
+int start_listening (int portreq) {
+    
+    string port(to_string(portreq)); 
+    struct addrinfo hints, *res, *p;
+    int sockfd, connectedfd = -1;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;       // use the IP of this machine
+
+    int status = getaddrinfo(NULL, port.c_str(), &hints, &res);
+    if (status == -1) {
+        cerr << "start_listening error: getaddrinfo" << endl;
+        return 2;
+    }
+
+    
+    int usable_addresses = 0;
+    for (p = res; p != NULL; p = p->ai_next) {
+        usable_addresses++;
+        
+        //cout << "Address info : ";
+        char ipaddrstr[INET6_ADDRSTRLEN];
+        inet_ntop(p->ai_family, &((struct sockaddr_in *)(res->ai_addr))->sin_addr, ipaddrstr, INET6_ADDRSTRLEN); 
+        //cout << ipaddrstr << endl;
+    } 
+    //cout << "getaddrinfo usable addresses : " << usable_addresses << endl;
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (status == -1) {
+        cerr << "start_listening error: socket" << endl;
+        return 2;
+    }
+
+    status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+    if (status == -1) {
+        close(sockfd);
+        cerr << "start_listening error: bind" << endl;
+        return 2;
+    }
+    
+    status = listen(sockfd, BACKLOG);
+    if (status == -1) {
+        close(sockfd);
+        cerr << "start_listening error: listen" << endl;
+        return 2;
+    }
+ 
+    cout << "Waiting for a connection on " << get_ip() << " port " << port << endl;
+
+    struct sockaddr_storage peeraddr;
+    socklen_t peeraddrsize = sizeof(peeraddr);
+
+    connectedfd = accept(sockfd, (struct sockaddr *)&peeraddr, &peeraddrsize);
+    if (status == -1) {
+        cerr << "start_listening error: accept" << endl;
+        return 2;
+    } else {
+        char peeraddrstr[INET6_ADDRSTRLEN];
+        inet_ntop(AF_INET, &((struct sockaddr_in*) &peeraddr)->sin_addr, peeraddrstr, INET6_ADDRSTRLEN);
+        //cout << "Good connection!" << endl;
+        //cout << "Connection from : " << peeraddrstr << endl;
+        cout << "Received connection..." << endl;
+    }
+    
+    freeaddrinfo(res);
+
+    close(sockfd);
+    close(connectedfd);
+    return 0;
+}
+
+
+string get_ip () {
+ 
+    string ip;
+
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+
+    char hostname[64];
+
+    gethostname(hostname, 64);
+    
+    //cout << "Getting hostname : " << hostname << endl;
+    status = getaddrinfo(hostname,  NULL, &hints, &res); 
+    if (status != 0) {
+        cerr << "getaddrinfo: " << gai_strerror(status) << endl;
+        return string("ERROR: check cerr");
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        void *addr;
+        string ipver;
+
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = string("IPv4");
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = string("IPv6");
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        //cout << ipver << " : " << ipstr << endl;
+
+    }
+
+    freeaddrinfo(res);
+
+    ip = string(ipstr);
+
+    return ip;
 }
